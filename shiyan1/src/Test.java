811327b25a6a74d879dee8c4e714ebec473620a9
bigweiyan <994334509@qq.com>
2016-10-25 13:01:43 +0800

    create lab4 and change without tools

--- shiyan1/src/Test.java
+++ shiyan1/src/Test.java
@@ -1,301 +1,277 @@
+
 /**
  * Created by think on 2016/9/28.
  */
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
 
 public class Test {
 
-    public String expression(String s){
-        int a[];
-        int n,m,k;
-        String[] arr;
-        k = 0;
-        for(int i = 0; i <s.length(); i++){
-            if  ((s.charAt(i)!='+')&&(s.charAt(i)!='*')){
-                if (Character.isLetterOrDigit( s.charAt(i) )==false) {
-                    k=1;
-                }
-            }
-            if (i>0){
-                if  (((s.charAt(i)=='+')||(s.charAt(i)=='*'))&&((s.charAt(i-1)=='+')||(s.charAt(i-1)=='*')))
-                {
-                    k=1;
-                }
-                if  ((Character.isLetterOrDigit( s.charAt(i) )==true)&&(Character.isLetterOrDigit( s.charAt(i-1) )==true))
-                {
-                    k=1;
-                    if ((Character.isDigit( s.charAt(i) )==true)&&(Character.isDigit( s.charAt(i-1) )==true)){
-                        k=0;
-                    }
-                }
-            }
-        }
-        if (k==1){
-            s="Error";
-        }
-        return s;
+	/**
+	 * 查看输入字符串是否合法 <br>
+	 * 规则：1）变量名为单个字符 2）只包含数字、字母、+和*的标准多项式
+	 * 
+	 * @param line
+	 *            输入字符串
+	 * @return 如果合法,则原样输出，否则输出error
+	 */
+	public String checkSyntax(String line) {
+		boolean error = false;
+		for (int i = 0; i < line.length(); i++) {
+			if ((line.charAt(i) != '+') && (line.charAt(i) != '*')) {
+				if (Character.isLetterOrDigit(line.charAt(i)) == false) {
+					error = true;
+				}
+			}
+			if (i > 0) {
+				if (((line.charAt(i) == '+') || (line.charAt(i) == '*'))
+						&& ((line.charAt(i - 1) == '+') || (line.charAt(i - 1) == '*'))) {
+					error = true;
+				}
+				if ((Character.isLetterOrDigit(line.charAt(i)) == true)
+						&& (Character.isLetterOrDigit(line.charAt(i - 1)) == true)) {
+					error = true;
+					if ((Character.isDigit(line.charAt(i))) && (Character.isDigit(line.charAt(i - 1)) == true)) {
+						error = false;
+					}
+				}
+			}
+		}
+		if (error) {
+			line = "Error";
+		}
+		return line;
+	}
 
-    }
-    public String putin(String s,String exp) {
-        int k = 0;
-        String x;
-        String y;
-        String[] arr;
-        arr = s.split(" ");
-        if (arr[0]!="!simplify")
-        {
-            k=1;
-        }
-        //System.out.println(exp+"+++");
-        for(int i = 1; i <arr.length; i++)
-        {
-            //System.out.println(exp);
-            String[] arr1=arr[i].split("=");
-            x=arr1[0];
-            y=arr1[1];
-            String[] str=exp.split("\\+");                        // exp   3*4*x+2*y
-            exp="";
-            for(int j = 0; j <str.length; j++)
-            {
-                //System.out.println(str[j]);                                                //  str   3*4*x   2*y
-                String[] str1=str[j].split("\\*");                 //str1   3 4 x     2 y
-                str[j]="";
-                for(int o = 0; o <str1.length; o++)              //!simplify x=22 y=1                 31*4*x+2*y
-                {
-                    if (str1[o].equals(x)==true)
-                    {
-                        str[j]=str[j]+y;
-                    }else
-                    {
-                        str[j]=str[j]+str1[o];
-                    }
-                    if (o<str1.length-1)
-                    {  str[j]=str[j]+"*"; }
-                }
-                exp=exp+str[j];
-                if (j<str.length-1)
-                {  exp=exp+"+"; }
-            }
-        }
-        //System.out.println(exp);              /////////////////////////////////////
+	/**
+	 * 将变量的值代入多项式
+	 * 
+	 * @param line
+	 *            输入的用于代入的表达式， polynomial 多项式
+	 * @return 如果合法,返回代入后的多项式，否则返回Error
+	 */
+	public String substitute(String line, String polynomial) {
+		String[] substitutionArr;
+		substitutionArr = line.split(" ");
+		if (!"!simplify".equals(substitutionArr[0])) {
+			return "Error";
+		}
+		for (int i = 1; i < substitutionArr.length; i++) {
+			String[] substitution = substitutionArr[i].split("=");
+			String variable = substitution[0];
+			String value = substitution[1];
+			String[] monomialArr = polynomial.split("\\+");
+			polynomial = "";
+			for (int j = 0; j < monomialArr.length; j++) {
+				String[] monomial = monomialArr[j].split("\\*");
+				monomialArr[j] = "";
+				for (int o = 0; o < monomial.length; o++) {
+					if (monomial[o].equals(variable)) {
+						monomialArr[j] = monomialArr[j] + value;
+					} else {
+						monomialArr[j] = monomialArr[j] + monomial[o];
+					}
+					if (o < monomial.length - 1) {
+						monomialArr[j] = monomialArr[j] + "*";
+					}
+				}
+				polynomial = polynomial + monomialArr[j];
+				if (j < monomialArr.length - 1) {
+					polynomial = polynomial + "+";
+				}
+			}
+		}
+		return polynomial;
+	}
 
-        return exp;
-    }
+	/**
+	 * 将表达式进行化简和格式化
+	 * 
+	 * @param exp
+	 *            表达式的字符串
+	 * @return 化简并格式化后的字符串
+	 */
+	public String simplify(String exp) {
+		String[] monomialList = exp.split("\\+");
+		String afterExp = ""; // 化简后的表达式
+		String monomial = "";
+		for (int i = 0; i < monomialList.length; i++) {
+			int constant = 1;
+			String[] factorList = monomialList[i].split("\\*");
+			monomial = "";
+			for (int j = 0; j < factorList.length; j++) {
+				if (Character.isDigit(factorList[j].charAt(0))) {
+					int factor = Integer.parseInt(factorList[j]);
+					constant = constant * factor;
+				} else {
+					monomial = monomial + "*" + factorList[j];
+				}
+			}
+			monomial = String.valueOf(constant) + monomial;
+			afterExp = afterExp + monomial;
+			if (i < monomialList.length - 1) {
+				afterExp = afterExp + "+";
+			}
+		}
 
-    public String simplify(String exp)
-    {
-        String[] str=exp.split("\\+");
-        exp="";
-        int q=1;
-        String exp1="";
-        for(int i = 0; i <str.length; i++)
-        {
-            q=1;
-            String[] str1=str[i].split("\\*");
-            exp1="";
-            for(int j = 0; j <str1.length; j++)
-            {
-                if (Character.isDigit( str1[j].charAt(0) )==true)
-                {
-                    int w=  Integer.parseInt(str1[j]);
-                    q= q * w;
-                }else
-                {
-                    exp1=exp1+"*"+str1[j];
-                }
-            }
-            exp1=String.valueOf(q)+exp1;
-            exp=exp+exp1;
-            if (i<str.length-1)
-            {  exp=exp+"+"; }
-        }
-//////////////////////////////////////////////////////////////////////////////////////////////////
-        exp="0+"+exp;
-        //System.out.println(exp); ////////////////////////////////////
-        str=exp.split("\\+");
-        exp="";
-        for(int i = 0; i <str.length; i++)
-        {
-            String[] str1=str[i].split("\\*");
-            //int r=0;
-            for(int j = 0; j <i; j++)
-            {
-                String[] str2=str[j].split("\\*");
-                int e=0;
-                if (str[j]==""){e=1;}
-                if (str1.length==str2.length)
-                {
-                    for(int o = 1; o <str1.length; o++)
-                    {
-                        if (str1[o].equals(str2[o])==false) {  e=1; }
-                    }
-                }else {e=1;}
-                if (e==0)
-                {
-                    str[i]="";
-                    str[j]="";
+		afterExp = "0+" + afterExp;
+		// Step1: 将所有的系数相乘
 
-                    int r=Integer.parseInt(str1[0]);
-                    int w=Integer.parseInt(str2[0]);
-                    str2[0]=String.valueOf(w+r);
-                    for (int o=0;o<str2.length;o++)
-                    {
-                        str[j]=str[j]+str2[o];
-                        if (o<str2.length-1)
-                        {  str[j]=str[j]+"*"; }
-                    }
-                }
-            }
-        }
-        for(int i = 0; i <str.length; i++)
-        {
-            String[] str1 = str[i].split("\\*");
-            str[i]="";
-            String a="1";
-            if (a.equals((str1[0]))==true)
-            {
-                str1[0]="";
-            }else
-            {
-                if (str1.length!=1) {str[i]=str1[0]+"*"; }
-                else {str[i]=str1[0];}
-            }
-            for(int j = 1; j <str1.length; j++)
-            {
-                str[i]=str[i]+str1[j];
-                if (j <str1.length-1)
-                {
-                    str[i]=str[i]+"*";
-                }
-            }
-        }
-        //System.out.println(exp+"     "+str.length);         //110  {} 487*y  {} {} {}
-        for(int i = 0; i <str.length; i++)
-        {
-            String a="0";
-            //System.out.print(str[i]+"/");
-            if (str[i]!="") {
-                if (a.equals((str[i]))==false) {
-                    //System.out.println(str[i] + "*" + i);
-                    if (i > 0) {
-                        if (i==1)
-                        {
-                            if (a.equals(str[0])==false){exp = exp + "+"; }
-                        }else
-                        {
-                            exp = exp + "+";
-                        }
-                    }
-                    exp = exp + str[i];
-                }
-            }
+		monomialList = afterExp.split("\\+");
+		afterExp = "";
+		for (int i = 0; i < monomialList.length; i++) {
+			String[] factorList = monomialList[i].split("\\*");
+			for (int j = 0; j < i; j++) {
+				String[] factorList2 = monomialList[j].split("\\*");
+				boolean isSame = true;
+				if ("".equals(monomialList[j])) {
+					isSame = false;
+				}
+				if (factorList.length == factorList2.length) {
+					for (int k = 1; k < factorList.length; k++) {
+						if (!factorList[k].equals(factorList2[k])) {
+							isSame = false;
+						}
+					}
+				} else {
+					isSame = false;
+				}
+				if (isSame) {
+					monomialList[i] = "";
+					monomialList[j] = "";
 
-        }
-        return exp;
+					int factor = Integer.parseInt(factorList[0]);
+					int factor2 = Integer.parseInt(factorList2[0]);
+					factorList2[0] = String.valueOf(factor + factor2);
+					for (int k = 0; k < factorList2.length; k++) {
+						monomialList[j] = monomialList[j] + factorList2[k];
+						if (k < factorList2.length - 1) {
+							monomialList[j] = monomialList[j] + "*";
+						}
+						// 由factorList重构monomial
+					}
+				}
+			}
+		}
+		// Step2：合并同类项
 
-    }
+		for (int i = 0; i < monomialList.length; i++) {
+			String[] factorList = monomialList[i].split("\\*");
+			monomialList[i] = "";
+			if ("1".equals((factorList[0])) && factorList.length > 1) {
+				factorList[0] = "";
+			} else {
+				if (factorList.length != 1) {
+					monomialList[i] = factorList[0] + "*";
+				} else {
+					monomialList[i] = factorList[0];
+				}
+			}
+			for (int j = 1; j < factorList.length; j++) {
+				monomialList[i] = monomialList[i] + factorList[j];
+				if (j < factorList.length - 1) {
+					monomialList[i] = monomialList[i] + "*";
+				}
+			}
+		}
+		// Step3：将系数为1项删除系数
 
-    public String der(String s,String exp)                  //4*x+y*x*x+y*2+y+x+z*x*y   !d/dx
-    {
-        String[] arr;
-        arr = s.split("d");
-        String x=arr[2];
-        String[] str=exp.split("\\+");
-        exp="";
-        for(int i = 0; i <str.length; i++)
-        {
-            int q=0;
-            String[] str1=str[i].split("\\*");
-            str[i]="";
-            for(int j = 0; j <str1.length; j++)
-            {
-                if (x.equals(str1[j])==true)
-                {
-                    q=q+1;
-                }
-            }
-            if (q==0)
-            {
-                str[i]="";
-            }else
-            {
-                int w=0;
-                for(int j = 0; j <str1.length; j++)
-                {
-                    if ((x.equals(str1[j])==true)&&(w==0))
-                    {
-                        w=1;
-                        str1[j]=String.valueOf(q);
-                    }
-                    str[i]=str[i]+str1[j];
-                    if (j <str1.length-1)
-                    {
-                        str[i]=str[i]+"*";
-                    }
-                }
-            }
-            if (str[i]!="") {
-                exp = exp + str[i];
-                if (i < str.length - 1) {
-                    exp = exp + "+";
-                }
-            }
-        }
-        return exp;
-    }
+		for (int i = 0; i < monomialList.length; i++) {
+			if (!"".equals(monomialList[i]) && !"0".equals((monomialList[i]))) {
+				afterExp = afterExp + monomialList[i];
+				afterExp = afterExp + "+";
+			}
+		}
+		if ("".equals(afterExp)) {
+			afterExp = "0+";
+		}
+		afterExp = afterExp.substring(0, afterExp.length() - 1);
+		// Step4：将项为0的情况删除
+		return afterExp;
 
+	}
 
+	/**
+	 * 对变量求导
+	 * 
+	 * @param line
+	 *            求导命令字符串 polynomial 原表达式
+	 * @return 返回求导后的多项式
+	 */
+	public String derivation(String line, String polynomial) // 4*x+y*x*x+y*2+y+x+z*x*y
+																// !d/dx
+	{
+		String varable = line.split("d")[2];
+		String[] monomialArr = polynomial.split("\\+");
+		polynomial = "";
+		for (int i = 0; i < monomialArr.length; i++) {
+			int power = 0;
+			String[] monomial = monomialArr[i].split("\\*");
+			monomialArr[i] = "";
+			for (int j = 0; j < monomial.length; j++) {
+				if (varable.equals(monomial[j]) == true) {
+					power++;
+				}
+			}
+			if (power == 0) {
+				monomialArr[i] = "";
+			} else {
+				boolean changed = false;
+				for (int j = 0; j < monomial.length; j++) {
+					if ((varable.equals(monomial[j]) == true) && !changed) {
+						changed = true;
+						monomial[j] = String.valueOf(power);
+					}
+					monomialArr[i] = monomialArr[i] + monomial[j];
+					if (j < monomial.length - 1) {
+						monomialArr[i] = monomialArr[i] + "*";
+					}
+				}
+			}
+			if (monomialArr[i].equals("") == false) {
+				polynomial = polynomial + monomialArr[i];
+				if (i < monomialArr.length - 1) {
+					polynomial = polynomial + "+";
+				}
+			}
+		}
+		return polynomial;
+	}
 
+	public static void main(String args[]) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		String line;
+		String polynomial = "";
+		String originalPoly = polynomial;
+		while (true) {
+			line = br.readLine();
+			Test test = new Test();
+			long start = System.nanoTime();
+			System.out.println("算法开始时间： " + start + "ns");
+			if (line.charAt(0) != '!') {
+				polynomial = test.checkSyntax(line);
+				polynomial = test.simplify(polynomial);
+				originalPoly = polynomial;
+			} else if (line.charAt(1) == 's') {
+				polynomial = test.substitute(line, originalPoly);
+				polynomial = test.simplify(polynomial);
+			} else if (line.charAt(1) == 'd') {
+				polynomial = test.derivation(line, originalPoly);
+				polynomial = test.simplify(polynomial);
+			} else {
+				polynomial = "error";
+			}
 
-    public static void main(String args[])
-            throws IOException
-    {
-        BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-        String s;
-        String exp="";
-        String exp1=exp;
-        while (true)
-        {
-            //System.out.println(s);
-            s=br.readLine();
-            Test test = new Test();
-            //String exp = "4*x+y*x*x+y*2+y+x+z*x*y";
-            //System.out.println(s);
-            long start=System.nanoTime();
-            System.out.println("算法开始时间： "+start+"ns");
-            if (s.charAt(0) != '!') {
-                exp = test.expression(s);
-                exp = test.simplify(exp);
-                exp1=exp;
-            } else if (s.charAt(1) == 's') {
-                exp = test.putin(s,exp1);
-                //System.out.println(exp);
-                exp = test.simplify(exp);
-            } else if (s.charAt(1) == 'd') {
-                exp = test.der(s,exp1);
-                //System.out.println(exp);
-                exp = test.simplify(exp);
-            }
-            else
-            {
-                exp = "error";
-            }
-            if(s.charAt(0) == '-')
-            {
-                break;
-            }
-            System.out.println(exp);
-            long end = System.nanoTime();
-            System.out.println("算法结束时间： "+end+"ns");
-            System.out.println("算法运行时间： "+(end-start)+"ns");
-            //System.out.println("****");
-            //4*x+y*x*x+y*2+y+x+z*x*y   !simplify x=22 y=1     !d/dx
-        }
+			if (line.charAt(0) == '-') {
+				break;
+			}
 
-    }
+			System.out.println(polynomial);
+			long end = System.nanoTime();
+			System.out.println("算法结束时间： " + end + "ns");
+			System.out.println("算法运行时间： " + (end - start) + "ns");
+		}
 
-
+	}
 }
